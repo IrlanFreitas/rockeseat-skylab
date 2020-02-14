b9e42a346c438f7941882847e1064c1cfe9cea58
Irlan <irlan.junior@solutis.com.br>
2020-02-14 17:07:37 -0300

    Buscando dados da api, armazenando no estado, listando produtos

--- CURSOREACT/huntweb/src/App.js
+++ CURSOREACT/huntweb/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "./components/Header/index";
 import './styles.css'
-
-// https://rocketseat-node.herokuapp.com/api
+import Main from './pages/main'
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
